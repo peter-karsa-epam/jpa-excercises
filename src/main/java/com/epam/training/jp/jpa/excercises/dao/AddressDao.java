c4bf27c989aef11ebe49d39363403b694b896773
Peter Veres2 <peter_veres2@epam.com>
2014-06-04 13:14:51 +0200

    fix AddressDao.save param type

--- src/main/java/com/epam/training/jp/jpa/excercises/dao/AddressDao.java
+++ src/main/java/com/epam/training/jp/jpa/excercises/dao/AddressDao.java
@@ -1,7 +1,9 @@
 package com.epam.training.jp.jpa.excercises.dao;
 
+import com.epam.training.jp.jpa.excercises.domain.Address;
+
 
 public interface AddressDao {
 
-	void save(Object address);
+	void save(Address address);
 }
--- src/main/java/com/epam/training/jp/jpa/excercises/dao/jpaimpl/JpaAddressDao.java
+++ src/main/java/com/epam/training/jp/jpa/excercises/dao/jpaimpl/JpaAddressDao.java
@@ -1,11 +1,12 @@
 package com.epam.training.jp.jpa.excercises.dao.jpaimpl;
 
 import com.epam.training.jp.jpa.excercises.dao.AddressDao;
+import com.epam.training.jp.jpa.excercises.domain.Address;
 
 public class JpaAddressDao extends GenericJpaDao implements AddressDao {
 
 	@Override
-	public void save(Object address) {
+	public void save(Address address) {
 		//TODO: implement
 		throw new UnsupportedOperationException();
 
